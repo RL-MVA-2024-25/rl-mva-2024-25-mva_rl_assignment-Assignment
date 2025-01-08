e3718766b9a82f881e1128aac980b3b4292214d5
antoine-martinez <antoine.martinez.x21@poltechnique.edu>
2025-01-08 15:07:19 +0100

    Third try

--- src/train.py
+++ src/train.py
@@ -1,7 +1,6 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
