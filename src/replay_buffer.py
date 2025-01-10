825a861872e6854f8c2ca5f40ce6c5ca0df479a4
Phuoc Ho <phuochoquang97@gmail.com>
2025-01-10 17:46:19 +0100

    add device to replay_buffer

--- src/replay_buffer.py
+++ src/replay_buffer.py
@@ -5,10 +5,11 @@ import torch
 
 # introduce the mechanism of experiment replay by storing the past samples
 class ReplayBuffer:
-    def __init__(self, capacity):
+    def __init__(self, capacity, device):
         self.capacity = capacity  # capacity of the buffer
         self.data = []
         self.index = 0  # index of the next cell to be filled
+        self.device = device
 
     def append(self, s, a, r, s_, d):
         if len(self.data) < self.capacity:
@@ -17,7 +18,10 @@ class ReplayBuffer:
         self.index = (self.index + 1) % self.capacity
 
     def sample(self, batch_size):
-        return random.sample(self.data, batch_size)
+        batch = random.sample(self.data, batch_size)
+        return list(
+            map(lambda x: torch.Tensor(np.array(x)).to(self.device), list(zip(*batch)))
+        )
 
     def __len__(self):
         return len(self.data)
